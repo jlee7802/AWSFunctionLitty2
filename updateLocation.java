b56958532de8b07dfa982a08b5f49d8d3da4bd0d
joseph7802 <jlee7802@gmail.com>
2018-06-02 17:09:38 -0400

    Added userLocation class and fixed logic to save user location when invoked from android studio

--- updateLocation.java
+++ updateLocation.java
@@ -2,44 +2,44 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
-import com.amazonaws.services.lambda.runtime.Context; 
 import java.time.LocalDateTime;
 //import updateUserLocationConfig;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class updateLocation implements RequestHandler<Object, String> {
+public class updateLocation implements RequestHandler<userLocation, String> {
 
-    public String handleRequest(Object input, Context context) {
-        String endpoint = updateUserLocationConfig.db_url;
-        String port = updateUserLocationConfig.db_port;
-        String db_name = updateUserLocationConfig.db_name;
-		String username = updateUserLocationConfig.db_username;
-		String pw = updateUserLocationConfig.db_password;
+    public String handleRequest(userLocation input, Context context) {
+        String endpoint = "";
+        String port = "";
+        String db_name = "";
+		String username = "";
+		String pw = "";
 
 		String url = "jdbc:postgresql://" + endpoint + ":" + port + "/" + db_name + "?user=" + username + "&password=" + pw;
-		LambdaLogger logger = context.getLogger();
 
         try {
         	Class.forName("org.postgresql.Driver");
 			Connection conn = DriverManager.getConnection(url);
 			Statement stmt = conn.createStatement();
 			LocalDateTime ldt = LocalDateTime.now();
-			stmt.executeQuery("UPDATE users SET latitude = 55, longitude = 66, updated_date = '" + String.valueOf(ldt) + "' WHERE user_id = " + "1");
+			stmt.executeQuery("UPDATE users SET latitude = " + input.getLatitude() + ", longitude = " + input.getLongitude() + ", updated_date = '" + String.valueOf(ldt) + "' WHERE user_id = " + input.getUserId());
+			stmt.close();
 			conn.close();
 			return "Success "+ url;
-		}
-		catch(ClassNotFoundException e){ 
-			return "classerror";
 		}
 		catch(SQLException e){
 			/*e.toString();
             log.warn(e.toString());
             System.out.println(e + "\nSQLException");*/
                             // Need to figure out what to do if there is an error
+            context.getLogger().log(e.getMessage());
             return "Failed Exception: " + e.getMessage() + "  getCause: " + e.getCause() + url + " " + username + " " + pw;
+		}
+		catch(Exception e){
+			context.getLogger().log("classerror1000" +e.getMessage());
+			return "classerror";
 		}
     }
 
