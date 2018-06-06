a6a0d78e1509b55c2fdbcf06c43d2a9a0904d1bf
joseph7802 <jlee7802@gmail.com>
2018-06-06 00:10:05 -0400

    Changed to Void return type

--- updateLocation.java
+++ updateLocation.java
@@ -8,9 +8,9 @@ import java.time.LocalDateTime;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class updateLocation implements RequestHandler<userLocation, String> {
+public class updateLocation implements RequestHandler<userLocation, Void> {
 
-    public String handleRequest(userLocation input, Context context) {
+    public Void handleRequest(userLocation input, Context context) {
         String endpoint = "";
         String port = "";
         String db_name = "";
@@ -19,7 +19,7 @@ public class updateLocation implements RequestHandler<userLocation, String> {
 
 		String url = "jdbc:postgresql://" + endpoint + ":" + port + "/" + db_name + "?user=" + username + "&password=" + pw;
 
-        try {
+        try{
         	Class.forName("org.postgresql.Driver");
 			Connection conn = DriverManager.getConnection(url);
 			Statement stmt = conn.createStatement();
@@ -27,7 +27,6 @@ public class updateLocation implements RequestHandler<userLocation, String> {
 			stmt.executeQuery("UPDATE users SET latitude = " + input.getLatitude() + ", longitude = " + input.getLongitude() + ", updated_date = '" + String.valueOf(ldt) + "' WHERE user_id = " + input.getUserId());
 			stmt.close();
 			conn.close();
-			return "Success "+ url;
 		}
 		catch(SQLException e){
 			/*e.toString();
@@ -35,12 +34,11 @@ public class updateLocation implements RequestHandler<userLocation, String> {
             System.out.println(e + "\nSQLException");*/
                             // Need to figure out what to do if there is an error
             context.getLogger().log(e.getMessage());
-            return "Failed Exception: " + e.getMessage() + "  getCause: " + e.getCause() + url + " " + username + " " + pw;
 		}
 		catch(Exception e){
 			context.getLogger().log("classerror1000" +e.getMessage());
-			return "classerror";
 		}
-    }
 
+		return null;
+    }
 }
